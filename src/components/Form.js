6f2d72a44f032d9282979b5006adbb0265a8b16f
Levi Winans <levieiko@gmail.com>
2019-07-22 18:24:27 -0700

    clean up old code

--- src/components/Form.js
+++ src/components/Form.js
@@ -110,103 +110,24 @@ export default class Form extends Component {
             <h1>Create Your Own Hot Sauce Here</h1>
             <form onSubmit={(e) => { submitForm(e, this.state)}}>
             <button type="submit">SAVE</button>
-                {/* <button name="click1" className={(click1 ? "toggleOn" : "offBtn")} onClick={(e) => {this.setToggle(e)}} type="button">Button 1</button> */}
-                {/* <button name="click2" className={(click2 ? "toggleOn" : "offBtn")} onClick={(e) => {this.setToggle(e)}} type="button">Button 2</button> */}
+
                 <div className="chiliSection">
                     {chiliList}
                 </div>
                 <div className="spiceSection">
                     {spiceList}
-                    {/* <section>
-                        <button name="spice1" className={(spice1 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Mex</h3><ul><li>salt</li><li>pepper</li></ul></section>
-                    </section>
-                    <section>
-                        <button name="spice2" className={(spice2 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Louisiana</h3><ul><li>salt</li><li>pepper</li></ul></section>
-                    </section>
-                    <section>
-                        <button name="spice3" className={(spice3 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Thai</h3><ul><li>salt</li><li>pepper</li></ul></section>
-                    </section>
-                    <section>
-                        <button name="spice4" className={(spice4 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Indian</h3><ul><li>salt</li><li>pepper</li></ul></section>
-                    </section>
-                    <section>
-                        <button name="spice5" className={(spice5 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Californian</h3><ul><li>salt</li><li>pepper</li></ul></section>
-                    </section> */}
+                   
                 </div>
                 <div className="extraSection">
                 {extraList}
-                    {/* <section>
-                        <button name="extra1" className={(extra1 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Fresh Garlic</h3></section>
-                    </section>
-                    <section>
-                        <button name="extra2" className={(extra2 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Fresh Onion</h3></section>
-                    </section>
-                    <section>
-                        <button name="extra3" className={(extra3 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Extras THREE</h3></section>
-                    </section>
-                    <section>
-                        <button name="extra4" className={(extra4 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Extras FOUR</h3></section>
-                    </section>
-                    <section>
-                        <button name="extra5" className={(extra5 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Extras FIVE</h3></section>
-                    </section> */}
+                   
                 </div>
                 <div className="vinegarSection">
                 {vinegarList}
-                    {/* <section>
-                        <button name="vinegar1" className={(vinegar1 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Vinegar ONE</h3></section>
-                    </section>
-                    <section>
-                        <button name="vinegar2" className={(vinegar2 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Vinegar TWO</h3></section>
-                    </section>
-                    <section>
-                        <button name="vinegar3" className={(vinegar3 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Vinegar THREE</h3></section>
-                    </section>
-                    <section>
-                        <button name="vinegar4" className={(vinegar4 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Vinegar FOUR</h3></section>
-                    </section>
-                    <section>
-                        <button name="vinegar5" className={(vinegar5 ? "toggleOn chiliBtn" : "chiliBtn")} onClick={(e) => {this.setToggle(e)}} type="button"></button>
-                        <section><h3>Vinegar FIVE</h3></section>
-                    </section> */}
+                  
                 </div>
 
 
-                {/* This was with a basic toggle on the classList */}
-                {/* <div className="chiliSection">
-                    <section>
-                        <div name="chili 1" className="chiliBtn" onClick={this.toggle}></div>
-                        <section><img src="red-pin.png" alt="placeholder"/><br/>Chili 1</section>
-                    </section>
-                    <section>
-                        <div name="chili 2" className="chiliBtn" onClick={this.toggle}></div>
-                        <section><img src="green-pin.png" alt="placeholder"/><br/>Chili 2</section>
-                    </section>
-                </div>
-                <div className="spiceSection">
-                    <section>
-                        <div name="spice 1" className="spiceBtn" onClick={this.toggle}></div>
-                        <section><img src="red-pin.png" alt="placeholder"/><br/>Spice 1</section>
-                    </section>
-                    <section>
-                        <div name="spice 2" className="spiceBtn" onClick={this.toggle}></div>
-                        <section><img src="green-pin.png" alt="placeholder"/><br/>Spice 2</section>
-                    </section>
-                </div> */}
 
             </form>
             </>
