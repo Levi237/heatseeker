7adfb58c254b0d6cddd08add211d250b2f5e302b
Levi Winans <levieiko@gmail.com>
2019-08-31 11:21:30 -0700

    env update

--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ import 'firebase/firestore';
     apiKey: "process.env.API_KEY",
     authDomain: "process.env.AUTH_DOMAIN",
     databaseURL: "process.env.DATABASE_URL",
-    projectId: "process.env.PROJECT_ID",
+    projectId: "heatseeker-custom",
     storageBucket: "process.env.STORAGE_BUCKET",
-    messagingSenderId: "process.env.MESSAGE_SENDER_ID",
+    messagingSenderId: "process.env.MESSAGING_SENDER_ID",
     appId: "process.env.APP_ID"
   });
 
