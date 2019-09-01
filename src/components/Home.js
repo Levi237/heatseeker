e4207e2eb50afe591bfc30761ffa069d942611fa
Levi Winans <levieiko@gmail.com>
2019-08-31 20:07:46 -0700

    set conditions for list

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,24 +10,29 @@ export default class Home extends Component {
     render(){
         const { recipes } = this.props
         let currentUser = firebase.auth().currentUser;
-        // let list = []
+        let listList = []
+        if (currentUser && recipes){
+
+
         let list = recipes.map((e, i) => {
             if (e.creator) {
                 let x = e.creator
-                console.log(currentUser.email, "currentUser.email", e)
+                let y = e.chili
+                console.log(currentUser.email, "currentUser.email", recipes)
                 if (x.email === currentUser.email) {
                     return(
-                        <div key={i}>{e.id}</div>
+                        <div key={i}><img src={`../chilis/${y.src}`} /></div>
                     )
                 }
             }
             })
-
+            listList.push(list)
+        }
         
         return(
             <div className="userHome">
                 {currentUser && currentUser.displayName}, Welcome Home, you are logged in right now
-            <br />{list}<br />
+            <br />{listList}<br />
                 <Username />
             </div>
         )
