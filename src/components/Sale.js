ffbfe2be9285a4ed8a9d1fe8e02f87b06a8eb192
Levi Winans <levieiko@gmail.com>
2019-09-03 07:32:19 -0700

    reset double selection

--- src/components/Sale.js
+++ src/components/Sale.js
@@ -28,7 +28,7 @@ export default class Sale extends Component {
             let nre = newRecipe.extra
                 const addExtra = nre.map((data, i) => {
                     return(
-                        <li key={i}>{data}</li>
+                        <li key={i}>{data.name}</li>
                     )
                 })
                 addExtras.push(addExtra)
@@ -39,33 +39,36 @@ export default class Sale extends Component {
                 )
             })
             showSpices.push(showSpice)
-
         }
 
         return(
             <>
-            {   newRecipe &&
+            {newRecipe &&
             <>
             {/* <Enter login={login} onClose={this.showModal}></Enter> */}
-
                 <h2>Your Recipe</h2>
                 <div className="new-recipe">
-                { newRecipe.chili[1] ? 
-<progress className="bored-bar" value={(newRecipe.chili[0].heat + newRecipe.chili[1].heat)/2} max="15"></progress>
-: 
-<progress className="bored-bar" value={(newRecipe.chili[0].heat)} max="15"></progress>           }
-                <img src={`../chilis/${newRecipe.chili.src}`} alt={newRecipe.chili.name} />
+                { newRecipe.chili[1] 
+                ? <progress className="bored-bar" value={(newRecipe.chili[0].heat + newRecipe.chili[1].heat)/2} max="15"></progress>
+                : <progress className="bored-bar" value={(newRecipe.chili[0].heat)} max="15"></progress> 
+                }
+                
                     <div className="show-recipe">
-                        { newRecipe.chili[1] 
-                        ? <><span>Pepper:</span><section><strong>{ newRecipe.chili[0].name } & { newRecipe.chili[1].name }</strong></section></>
-                        : <><span>Pepper:</span><section><strong>{ newRecipe.chili[0].name }</strong></section></>
+                        { newRecipe.chili[1]
+                        ? <>
+                        <img src={`../chilis/${newRecipe.chili[0].src}`} alt={newRecipe.chili[0].name} className="chili"/><img src={`../chilis/${newRecipe.chili[1].src}`} alt={newRecipe.chili[1].name} className="chili"/><br/><span>Pepper:</span><section><strong>{ newRecipe.chili[0].name } & { newRecipe.chili[1].name }</strong></section>
+                        </>
+                        : <>
+                        <img src={`../chilis/${newRecipe.chili[0].src}`} alt={newRecipe.chili[0].name} className="chili"/><span>Pepper:</span><section><strong>{ newRecipe.chili[0].name }</strong></section>
+                        </>
                         }
-                        <img className="chalk" src="chalkdarkorange.png" alt="line break"/>
+                        <img className="chalk-line" src="chalkdarkorange.png" alt="line break"/>
                         <span>Spice:</span>{ newRecipe.spice.name && <section><strong>{ newRecipe.spice.name } Spice</strong></section>}
                         { newRecipe.spice.name && <ul>{ showSpices }</ul>}
-                        
-                        { newRecipe.extra[0] ? <><img className="chalk" src="chalkdarkorange.png" alt="line break"/><section><span>Add On:</span></section><br /><ul>{ addExtras }</ul></> : ""}
-                        <img className="chalk" src="chalkdarkorange.png" alt="line break"/>
+                        { newRecipe.extra[0] 
+                        ? <><img className="chalk-line" src="chalkdarkorange.png" alt="line break"/><section><span>Add On:</span></section><br /><ul>{ addExtras }</ul></> 
+                        : ""}
+                        <img className="chalk-line" src="chalkdarkorange.png" alt="line break"/>
                         <span>Vinegar:</span>{ newRecipe.vinegar.name && <section><strong>{ newRecipe.vinegar.name }</strong></section>}
                     </div>
                     <h3>Total: ${(newRecipe.chili[0].price)/100}.00</h3>
