e563de5eb61c55b09b82f83042c7adf48da5c2c1
Levi Winans <levieiko@gmail.com>
2019-09-07 14:01:34 -0700

    adjust username on show.js display

--- src/components/modal/Show.js
+++ src/components/modal/Show.js
@@ -78,71 +78,66 @@ export default class Show extends Component {
         return(
             <>
             {recipe &&
-            <>
-            {
-                (!user && this.state.login) ?
-            <Enter /> :""
-            }
-            {
-                user &&  <>{user.displayName}</>
-            }
-                <h2>Your Recipe</h2><br/>
-                { recipe.chili[1]
-                        ? <>
-                        <img src={`../chilis/${recipe.chili[0].src}`} alt={recipe.chili[0].name} className="chili"/>
-                        <h3>{recipe.style}</h3>
-                        <img src={`../chilis/${recipe.chili[1].src}`} alt={recipe.chili[1].name} className="chili"/>
-                        {/* <br/><span>Pepper:</span><section><strong>{ recipe.chili[0].name } & { recipe.chili[1].name }</strong></section> */}
-                        </>
-                        : <>
-                        <img src={`../chilis/${recipe.chili[0].src}`} alt={recipe.chili[0].name} className="chili"/>
-                        <h3>{recipe.style}</h3>
-                        <img src={`../chilis/${recipe.chili[0].src}`} alt={recipe.chili[0].name} className="chili"/>
-                        {/* <span>Pepper:</span><section><strong>{ recipe.chili[0].name }</strong></section> */}
-                        </>
-                        }
-                
-                <div className="new-recipe">
-                { recipe.chili[1] 
-                ? <progress className="bored-bar" value={(recipe.chili[0].heat + recipe.chili[1].heat)/2} max="15"></progress>
-                : <progress className="bored-bar" value={(recipe.chili[0].heat)} max="15"></progress> 
+                <>
+                {
+                    (!user && this.state.login) &&
+                    <Enter />
                 }
-                
-                    <div className="show-recipe">
-                        { recipe.chili[1]
-                        ? <>
-                        <span>Pepper:</span><section><strong>{ recipe.chili[0].name } & { recipe.chili[1].name }</strong></section>
-                        </>
-                        : <>
-                        <span>Pepper:</span><section><strong>{ recipe.chili[0].name }</strong></section>
-                        </>
-                        }
-                        <img className="chalk-line" src="chalkdarkorange.png" alt="line break"/>
-                        <span>Spice:</span>{ recipe.spice.name && <section><strong>{ recipe.spice.name } Spice</strong></section>}
-                        { recipe.spice.name && <ul>{ showSpices }</ul>}
-                        { recipe.extra[0] 
-                        ? <><img className="chalk-line" src="chalkdarkorange.png" alt="line break"/><section><span>Add On:</span></section><br /><ul>{ addExtras }</ul></> 
-                        : ""}
-                        <img className="chalk-line" src="chalkdarkorange.png" alt="line break"/>
-                        <span>Vinegar:</span>{ recipe.vinegar.name && <section><strong>{ recipe.vinegar.name }</strong></section>}
-                    </div><br/>
-
-
-
-                    { (user && !newRecipe) && <button><a href="/my-home">Return Home</a></button> }
-                    { (user && newRecipe )
-                    ? <button onClick={() => {this.saveForm(); clearNewRecipe()}}>Return Home</button>//<a href="/my-home">Return Home</a>
-                    // : <button> Save to Account</button>
-                    : ""
+                {
+                    user ?  <h2>{user.displayName}'s Recipe</h2> : <h2>Your Recipe</h2>
                 }
-                 { (!user && newRecipe ) && <button onClick={this.showEnter}> Save to Account</button> }
-                    <button><a href="/order">Continue with Order</a></button>
-                    {/* <button><a href="/my-home">Back Home</a></button> */}
-                    {/* :   <Redirect to={'/home'} />  */}
+                    <br/>
+                    { recipe.chili[1]
+                            ? <>
+                            <img src={`../chilis/${recipe.chili[0].src}`} alt={recipe.chili[0].name} className="chili"/>
+                            <h3>{recipe.style}</h3>
+                            <img src={`../chilis/${recipe.chili[1].src}`} alt={recipe.chili[1].name} className="chili"/>
+                            </>
+                            : <>
+                            <img src={`../chilis/${recipe.chili[0].src}`} alt={recipe.chili[0].name} className="chili"/>
+                            <h3>{recipe.style}</h3>
+                            <img src={`../chilis/${recipe.chili[0].src}`} alt={recipe.chili[0].name} className="chili"/>
+                            </>
+                    }
+                    
+                    <div className="new-recipe">
+                    { recipe.chili[1] 
+                    ? <progress className="bored-bar" value={(recipe.chili[0].heat + recipe.chili[1].heat)/2} max="15"></progress>
+                    : <progress className="bored-bar" value={(recipe.chili[0].heat)} max="15"></progress> 
+                    }
+                    
+                        <div className="show-recipe">
+                            { recipe.chili[1]
+                            ? <>
+                            <span>Pepper:</span><section><strong>{ recipe.chili[0].name } & { recipe.chili[1].name }</strong></section>
+                            </>
+                            : <>
+                            <span>Pepper:</span><section><strong>{ recipe.chili[0].name }</strong></section>
+                            </>
+                            }
+                            <img className="chalk-line" src="chalkdarkorange.png" alt="line break"/>
+                            <span>Spice:</span>{ recipe.spice.name && <section><strong>{ recipe.spice.name } Spice</strong></section>}
+                            { recipe.spice.name && <ul>{ showSpices }</ul>}
+                            { recipe.extra[0] 
+                            ? <><img className="chalk-line" src="chalkdarkorange.png" alt="line break"/><section><span>Add On:</span></section><br /><ul>{ addExtras }</ul></> 
+                            : ""}
+                            <img className="chalk-line" src="chalkdarkorange.png" alt="line break"/>
+                            <span>Vinegar:</span>{ recipe.vinegar.name && <section><strong>{ recipe.vinegar.name }</strong></section>}
+                        </div><br/>
+
+                        { (user && !newRecipe) && 
+                            <button><a href="/my-home">Return Home</a></button> }
+                        { (user && newRecipe ) && 
+                            <button onClick={() => {this.saveForm(); clearNewRecipe()}}>Return Home</button> }
+                        { (!user && newRecipe ) && 
+                            <button onClick={this.showEnter}> Save to Account</button> }
+
+                        <button><a href="/order">Continue with Order</a></button>
+
                     </div>
-                    </>
-                }
                 </>
+            }
+            </>
     
         )
     }
