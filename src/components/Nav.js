fa4289cb500bbdd8e4b27cba426a6a60b3a8a84a
Levi Winans <levieiko@gmail.com>
2019-09-07 14:28:16 -0700

    remove CREATE limit on nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -6,10 +6,7 @@ import * as routes from '../constants/routes'
 const Nav = ({newRecipe, user, logout}) => 
     <div className="navBox">
         <NavLink activeClassName="" to={routes.INFO}>ABOUT</NavLink>
-
-        { !newRecipe 
-            && <NavLink activeClassName="" to={routes.FORM}>CREATE</NavLink>
-        }
+        <NavLink activeClassName="" to={routes.FORM}>CREATE</NavLink>
         <NavLink activeClassName="" to={routes.USER}>HOME</NavLink>
         { user 
             ?  <NavLink activeClassName="" to={routes.ROOT} onClick={logout}>LOGOUT</NavLink> 
