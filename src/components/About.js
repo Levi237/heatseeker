28b9effebe90ddee3b9b7a56b26b75693ae095f4
Levi Winans <levieiko@gmail.com>
2019-09-09 20:21:35 -0700

    push mvp with About addition

--- src/components/About.js
+++ src/components/About.js
@@ -3,9 +3,20 @@ import React from 'react';
 const About = () => 
             <div className="about">
                 <h2>Heatseeker's Create Your Own Hot Sauce Site</h2>
+                <h4>Desktop Version</h4>
                 <p>The purpose of this website is to allow customers to create personalized hotsauces in small batches.
                 <br />From gifts, to parties, to restaruant tables, this is a great way for people to express their own taste.
                 <br />Review the current price of your recipe before placing an order as prices may have changed since it was last saved.
+                </p>
+                <p>
+                    <h3>Need help getting started?  </h3><br/>
+                    Select one of the examples from the first, top row.<br />
+                    Select up to two Chilis. <br />
+                    Pick a Spice Style<br />
+                    Add other Flavors<br />
+                    Pick a Vinegar<br />
+                    Name your Hot Sauce!<br />
+                    EVENTUALLY add a Label and place your Order!!!<br />
                 </p>
             </div>  
 
