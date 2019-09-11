8aa96bc44c749a50885449cdd27d135ae5963bdc
Levi Winans <levieiko@gmail.com>
2019-09-11 11:12:51 -0700

    styling to show.js in mobile form

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => 
             <div className="about">
                 <h2>Heatseeker's Create Your Own Hot Sauce Site</h2>
-                <h4>Desktop Version</h4>
+                <h4>Desktop/Mobile</h4>
                 <p>The purpose of this website is to allow customers to create personalized hotsauces in small batches.
                 <br />From gifts, to parties, to restaruant tables, this is a great way for people to express their own taste.
                 <br />Review the current price of your recipe before placing an order as prices may have changed since it was last saved.
