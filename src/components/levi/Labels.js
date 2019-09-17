2213950f68a60fac51c4c49aae9a6752e4d75fe4
Levi Winans <levieiko@gmail.com>
2019-09-16 23:19:13 -0700

    add ternarery to labels

--- src/components/levi/Labels.js
+++ src/components/levi/Labels.js
@@ -5,30 +5,31 @@ import '../Home.css'
 
 export default class Labels extends Component {
 render(){
+    const { user } = this.props
 
 
         return(
             <div className="labels overflow-list">
                 <div className="label1">
-                    <h3>{this.props.user.displayName}'s</h3>
+                    <h3>{user ? `${user.displayName}'s` : "HEATMAKERS"}</h3>
                     <img src="chili-burn.png" />
                     <h4>Hot Sauce</h4>
                 </div>
                 
                 <div className="label2">
-                    <h3>{this.props.user.displayName}'s</h3>
+                    <h3>{user ? `${user.displayName}'s` : "HEATMAKERS"}</h3>
                     <img src="real-chili.jpg" />
                     <h4>Hot Sauce</h4>
                 </div> 
 
                 <div className="label3">
-                    <h3>{this.props.user.displayName}'s</h3>
+                    <h3>{user ? `${user.displayName}'s` : "HEATMAKERS"}</h3>
                     <img src="chili-outline-bw-line.png" />
                     <h4>Hot Sauce</h4>
                 </div> 
                 
                 <div className="label4">
-                    <h3>{this.props.user.displayName}'s</h3>
+                    <h3>{user ? `${user.displayName}'s` : "HEATMAKERS"}</h3>
                     <img src="../chili-logo.png" />
                     <h4>Hot Sauce</h4>
                 </div>
