3a1d7b1d7431e20238c211c143a74c28a9bee45c
Levi Winans <levieiko@gmail.com>
2019-09-19 13:47:50 -0700

    update About

--- src/components/About.js
+++ src/components/About.js
@@ -7,6 +7,7 @@ const About = () =>
         <p>The purpose of this website is to allow customers to create personalized hotsauces in small batches.
         <br />From gifts, to parties, to restaruant tables, this is a great way for people to express their own taste.
         </p>
+        
         <h3>Current Function:</h3>
             <section>Create a hotsauce recipe and name it</section>
             <section>Provide default recipe labels</section>
@@ -14,7 +15,6 @@ const About = () =>
             <br/>
                         
         <h3>Current Development:</h3>
-        
             <section>Info/Help Modal  to explain process</section>
             <section>Enable edit of recipes</section>
             <section>Integrate label maker into create form.js and show.js</section>
@@ -22,10 +22,23 @@ const About = () =>
             <br/>
         
         <h3>Points of Pride:</h3>
-        
             <section>Photoshop images including header logo made from screen shot of font</section>
             <section>Full Stack w/ Firebase | React | CSS | JS</section>
             <section>Minimal imports</section>
+        
+        <h3>Future Goals:</h3>
+            <section>eCommerce</section>
+            <section>Label upload</section>
+            <section>Recipe Cards</section>
+            <section>Menu toggle</section>
+            <section>Mobile Adjustments</section>
+        
+        <h3>Ultimate Goal:</h3>
+            <section>eCommerce</section>
+
+        <br/>
+
+
     </div>  
 
 export default About
