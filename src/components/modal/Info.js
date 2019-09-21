dc879cdd457a6a7a693287b53f2a97d76cb77a1b
Levi Winans <levieiko@gmail.com>
2019-09-20 19:43:57 -0700

    stayl info, add bullets, clean up content

--- src/components/modal/Info.js
+++ src/components/modal/Info.js
@@ -1,29 +1,26 @@
 import React from 'react';
-
 const Info = () => 
-    <div>
+    <div className="info-container">
         <h1>WELCOME TO HEATMAKERS</h1>
-
         <h2>Create your own hotsauce right here!</h2>
-
-        <p>The purpose of this website is to allow customers to create personalized hotsauces in small batches.
-        <br />From gifts, to parties, to restaruant tables, this is a great way for people to express their own taste.
+        <p>
+            The purpose of this website is to allow customers to create personalized hotsauces in small batches.
+            <br />
+            From gifts, to parties, to restaruant tables, this is a great way for people to express their own taste.
         </p>
-
-        <h3>Need help getting started?  </h3><br/>
-        Select one of the examples from the first, top row.<br />
+        <h3>Need help getting started? </h3><br/>
+            Select one of the examples from the top row.<br />
         <h3>Know what you want?</h3><br/>
             <ul>
-                <li>Select up to two Chilis. </li>
+                <li>Select up to two Chilis</li>
                 <li>Pick a Spice Style</li>
                 <li>Add other Flavors</li>
                 <li>Pick a Vinegar</li>
-                <li>Choose one of 4 Label styles</li>
-                <li>liand your bottle</li>
-                <li>Name your Hot Sauce!</li>
+                <li>Choose 1 of 4 Label styles</li>
+                <li>Brand your bottle</li>
+                <li>Name your Hot Sauce</li>
             </ul>
-
-        EVENTUALLY add upload a Label, include Pricing and place your Order!!!<br />
+            EVENTUALLY add upload a Label, include Pricing and place your Order!!!<br />
         <br/>
     </div>
 export default Info
