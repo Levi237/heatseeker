f5ab469d91d4312c98ea825a268ac91dbed93fcc
Levi Winans <levieiko@gmail.com>
2019-09-24 01:01:54 -0700

    create master form for create and update

--- src/components/MasterForm.js
+++ src/components/MasterForm.js
@@ -179,6 +179,54 @@ export default class Form extends Component {
         // const { chilis, spices, extras, vinegars, user, closeEditForm } = this.props
         const { chili, spice, vinegar, extra, examples, style, label, icon, header, close, examplesVisibility } = this.state
         const { chilis, spices, extras, vinegars, edit, submitForm, newRecipe, user, closeEditForm } = this.props
+        let chili1 = ""; if ( chili[0] ){ chili1 = chili[0] }  
+        let chili2 = ""; if ( chili[1] ){ chili2 = chili[1] } 
+        let extra1 = ""; if ( extra[0] ){ extra1 = extra[0] } 
+        let extra2 = ""; if ( extra[1] ){ extra2 = extra[1] } 
+        let extra3 = ""; if ( extra[2] ){ extra3 = extra[2] } 
+        let extra4 = ""; if ( extra[3] ){ extra4 = extra[3] } 
+        let extra5 = ""; if ( extra[4] ){ extra5 = extra[4] } 
+        let extra6 = ""; if ( extra[5] ){ extra6 = extra[5] } 
+        let extra7 = ""; if ( extra[6] ){ extra7 = extra[6] }
+
+        const chiliList = chilis.map((ch, key) => {
+            return(
+                <section className="chiliSection" key={key}>
+                    <button name="chili" value={ch} className={(chili1.id === ch.id || chili2.id === ch.id ? "toggleOn select-btn" : "select-btn")} onClick={(e) => {this.chiliToggle(e, ch)}} type="button"></button>
+                    <section><img src={`../chilis/${ch.src}`} alt={ch.name}/><br/>{ch.name}</section>
+                </section>
+            )
+        })
+        const spiceList = spices.map((sp, i) => {
+            const spiceItems= sp.items.map((item, key) => {
+                return (
+                    <li key={key}>{item}</li>
+                )
+            })
+            return (
+                <section className="spiceSection" key={i}>
+                
+                    <button name="spice" value={sp} className={(spice.id === sp.id ? "toggleOn select-btn" : "select-btn")} onClick={(e) => {this.setToggle(e, sp)}} type="button"></button>
+                    <section>{sp.name}<ul>{spiceItems}</ul></section>
+                </section>
+            )
+        })
+        const extraList = extras.map((ex, key) => {
+            return (
+                <section className="chiliSection" id={`${ex.name}`} key={key}>
+                    <button name="extra" value={ex} className={(extra1.id === ex.id || extra2.id === ex.id || extra3.id === ex.id || extra4.id === ex.id || extra5.id === ex.id || extra6.id === ex.id || extra7.id === ex.id ? "toggleOn select-btn" : "select-btn")} onClick={(e) => {this.extraToggle(e, ex)}} type="button"></button>
+                    <section><img src={`../extras/${ex.img}`} alt={`${ex.name}`}/><br/>{ex.name}</section>
+                </section>
+            )
+        })
+        const vinegarList = vinegars.map((vi, key) => {
+            return(
+                <section className="chiliSection" key={key}>
+                    <button name="vinegar" className={(vinegar.name === vi.name? "toggleOn select-btn" : "select-btn")} onClick={(e) => {this.setToggle(e, vi)}} type="button"></button>
+                    <section><img src={`../vinegars/${vi.img}`} alt={`${vi.name}`}/><br/>{vi.name}</section>
+                </section>
+            )
+        })
 
         const showExamples = examples.map((ex, i) => {
             return(
@@ -214,20 +262,17 @@ export default class Form extends Component {
                 : <progress className="bored-bar" value={chili[0] ? (chili[0].heat) : 0} max="15"></progress>           
                 }
                 </div>
-                <Select 
-                    chili={chili} 
-                    spice={spice} 
-                    vinegar={vinegar }
-                    extra={extra} 
-                    chilis={chilis}
-                    spices={spices} 
-                    extras={extras} 
-                    vinegars={vinegars} 
-                    setToggle={this.setToggle}
-                    chiliToggle={this.chiliToggle}
-                    extraToggle={this.extraToggle}
-                    />
-            </div>   
+                <div className="chiliSection">
+                    <ScrollMenu data={chiliList} arrowLeft={ArrowLeft} arrowRight={ArrowRight}/>
+                </div>
+                    <img className="chalk" src="chalkdarkorange.png" alt="line break"/>  
+                    <ScrollMenu data={spiceList} arrowLeft={ArrowLeft} arrowRight={ArrowRight}/>
+                    <img className="chalk" src="chalkdarkorange.png" alt="line break"/>  
+                    <ScrollMenu data={extraList} arrowLeft={ArrowLeft} arrowRight={ArrowRight}/>       
+                    <img className="chalk" src="chalkdarkorange.png" alt="line break"/>  
+                    <ScrollMenu data={vinegarList} arrowLeft={ArrowLeft} arrowRight={ArrowRight}/> 
+                    <img className="chalk" src="chalkdarkorange.png" alt="line break"/>  
+                </div>   
 
             <div className="box1">
                 <div className="pick-label">
