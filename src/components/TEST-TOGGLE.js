b1486122b018597cba42728d3dc45b8c2400bdca
Levi Winans <levieiko@gmail.com>
2019-10-08 13:55:07 -0700

    framework 1 complete

--- src/components/TEST-TOGGLE.js
+++ src/components/TEST-TOGGLE.js
@@ -3,12 +3,12 @@ import React from 'react';
 const TestToggle = () => {
     return (
         <div className="label-toggle-container">
+            <div><input type="button" className="select-background" name="" /></div>
             <div><input type="button" className="select-outer-bars" name="" /></div>
             <div><input type="button" className="select-inner-bars" name="" /></div>
             <div><input type="button" className="select-borders" name=""></input><div className="select-outer-border"><div className="select-inner-border"></div></div></div>
-            <div><input value="T" type="button" className="select-text-color" name="" /></div>
-            <div><input value="Border Line" type="button" className="select" name="" /></div>
-            <div><input value="Name Bar" type="button" className="select" name="" /></div>
+            <div><input type="button" className="select-text-color" name="" value="T" /></div>
+            <div><input type="button" className="select-background" name="" /></div>
         </div>
     );
 };
