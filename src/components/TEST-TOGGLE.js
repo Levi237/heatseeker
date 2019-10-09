6d296e05baca3e588d044aa5555de8b517b40d80
Levi Winans <levieiko@gmail.com>
2019-10-09 13:24:14 -0700

    set background colors for selecter .bg-xxx

--- src/components/TEST-TOGGLE.js
+++ src/components/TEST-TOGGLE.js
@@ -9,7 +9,7 @@ const TestToggle = () => {
             <div><input type="button" className="select-frame" name="" /></div>
             <div><input type="button" className="select-bars" name="" /></div>
             <div><input type="button" className="select-borders" name=""></input><div className="select-outer-border"><div className="select-inner-border"></div></div></div>
-            <section><input type="button" className="text-icon select-text-color" name="" value="T" /></section>
+            <section><input type="button" className="text-icon select-text" name="" value="T" /></section>
             <section><input type="button" className="next-icon search-samples" name="" value="Themes" /></section>
             <section><input type="button" className="next-icon upload-images" name="" value="Upload" /></section>
             <section><input type="button" className="next-icon" name="" value="?" /></section>
