48a7dad2724f5e2399d8239ba9c2f95631e14d54
Levi Winans <levieiko@gmail.com>
2019-10-09 13:21:34 -0700

    adjust nomenclature

--- src/components/TEST-TOGGLE.js
+++ src/components/TEST-TOGGLE.js
@@ -4,10 +4,10 @@ const TestToggle = () => {
     return (
         <>
         <div className="label-toggle-container">
+            {/* <div><input type="button" className="select-background" name="" /></div> */}
             <div><input type="button" className="select-background" name="" /></div>
-            <div><input type="button" className="select-background" name="" /></div>
-            <div><input type="button" className="select-outer-bars" name="" /></div>
-            <div><input type="button" className="select-inner-bars" name="" /></div>
+            <div><input type="button" className="select-frame" name="" /></div>
+            <div><input type="button" className="select-bars" name="" /></div>
             <div><input type="button" className="select-borders" name=""></input><div className="select-outer-border"><div className="select-inner-border"></div></div></div>
             <section><input type="button" className="text-icon select-text-color" name="" value="T" /></section>
             <section><input type="button" className="next-icon search-samples" name="" value="Themes" /></section>
