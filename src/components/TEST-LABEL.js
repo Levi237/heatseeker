2f00d3eeb9773fafb1d0413cbf37b68963bd9400
Levi Winans <levieiko@gmail.com>
2019-10-10 17:01:16 -0700

    pass state to affect colors

--- src/components/TEST-LABEL.js
+++ src/components/TEST-LABEL.js
@@ -17,12 +17,15 @@ export default class TestLabel extends Component {
         background: "clear",
         topFrame: "clear",
         bottomFrame: "clear",
-        headerBar: "clear",
+        headerBar: "green",
+        headerColor: "pink",
         nameBar: "clear",
+        nameColor: "black",
         outerBorder: "clear",
         innerBorder: "clear",
     }
     render(){
+        const { background, topFrame, bottomFrame, headerBar, nameBar, outerBorder, innerBorder, nameColor, headerColor } = this.state
         const { icon, img, labelMaker, user, showModal } = this.props
 
 
@@ -30,19 +33,22 @@ export default class TestLabel extends Component {
             <>
                 <div className="label-maker-canvas">
                 <div className="label-maker-container bg-wood label-bg label-body">
-                    <div name="topFrame" className="top-bar bar-green"></div>
-                    <div name="bottomFrame" className="bottom-bar bar-green"></div>
-                    <div name="headerBar" className="header-bar bg-orangered"></div>
-                    <div name="nameBar" className="name-bar bg-white"></div>
-                    <div name="outerBorder" className="outer-border border-black"></div>
-                    <div name="innerBorder" className="inner-border border-red"></div>
+                    <div name="topFrame" className={`top-bar border-${topFrame}`}></div>
+                    {/* <div name="topFrame" className="top-bar border-green"></div> */}
+                    <div name="bottomFrame" className={`bottom-bar border-${bottomFrame}`}></div>
+                    {/* <div name="headerBar" className={`header-bar bg-${headerBar}`}></div> */}
+                    <div name="nameBar" className={`name-bar bg-${nameBar}`}></div>
+                    <div name="outerBorder" className={`outer-border border-${outerBorder}`}></div>
+                    <div name="innerBorder" className={`inner-border border-${innerBorder}`}></div>
 
                     <div className="label-content-container">
-                        <div className="label-header bar-green">
-                            <input className="font-black bg-red" name="header" type="text"/>
+                        <div name="headerBar" className={`label-header header-bar bg-${headerBar}`}>
+                            <input className={`text-${headerColor}`} name="header" type="text"/>
                         </div>
                         <img src={img ? img.url : icon} alt={img ? img.url : icon}/>  
-                        <input className="font-black label-name bg-yellow" name="style" type="text"/>        
+                        <div className={`name-bar bg-${nameBar} label-name}`} >
+                            <input name="nameBar" className={`name-bar label-name text-${nameColor}`} name="style" type="text"/>        
+                        </div>
                         <div className="label-oz font-black">5 FL.OZ - 147ml</div>
                     </div>
                 </div>
